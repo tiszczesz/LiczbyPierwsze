149102f2adaccb1eb82e8d6f3566e243a1831450
Pawe Szczeszek <pszczesz@gmail.com>
2019-12-07 09:25:50 +0100

    d1

--- LiczbyPierwsze.WebCoreApp1/Controllers/PrimaryNumbersController.cs
+++ LiczbyPierwsze.WebCoreApp1/Controllers/PrimaryNumbersController.cs
@@ -6,17 +6,27 @@ using LiczbyPierwsze.Main;
 using LiczbyPierwsze.WebCoreApp1.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace LiczbyPierwsze.WebCoreApp1.Controllers
-{
-    public class PrimaryNumbersController : Controller
-    {
-        public IActionResult Index()
-        {
+namespace LiczbyPierwsze.WebCoreApp1.Controllers {
+    public class PrimaryNumbersController : Controller {
+        public IActionResult Index() {
+            PrimaryModel pm = new PrimaryModel();
             return View();
         }
 
+        //POST
+        [HttpPost]
+        public IActionResult Index(PrimaryModel pm) {
+            if (ModelState.IsValid) {
+
+                return View();
+            }
+            else {
+                return View(pm);
+            }
+        }
+
         public IActionResult GetAll() {
-            PrimaryModel pm = new PrimaryModel(){DLimit = 2,ULimit = 200};
+            PrimaryModel pm = new PrimaryModel() {DLimit = 2, ULimit = 200};
             PrimaryNumbers pn = new PrimaryNumbers();
             pm.Numbers = pn.ShowPrimaryNumbers(pm.DLimit, pm.ULimit);
             return View(pm);
--- LiczbyPierwsze.WebCoreApp1/Models/PrimaryModel.cs
+++ LiczbyPierwsze.WebCoreApp1/Models/PrimaryModel.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using LiczbyPierwsze.Main;
+using System.ComponentModel.DataAnnotations;
 
 namespace LiczbyPierwsze.WebCoreApp1.Models
 {
     public class PrimaryModel
     {
-        
+        [Required(ErrorMessage = "Podaj dolną granicę")]
+        [Range(minimum:2,maximum:10000)]
+        [Display(Name = "Podaj dolną granicę")]
         public int DLimit { get; set; }
+
+        [Required(ErrorMessage = "Podaj górną granicę")]
+        [Range(minimum: 2, maximum: 10000)]
+        [Display(Name = "Podaj górną granicę")]
         public int ULimit { get; set; }
 
         public List<int> Numbers { get; set; }
